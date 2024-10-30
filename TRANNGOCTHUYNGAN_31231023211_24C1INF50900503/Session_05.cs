654329d3f96a1da9783d6b3dd30482ea05dba274
HP <HP@DESKTOP-9L5LHFE>
2024-10-30 15:30:25 +0700

    add session05

---
+++ TRANNGOCTHUYNGAN_31231023211_24C1INF50900503/Session_05.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRANNGOCTHUYNGAN_31231023211_24C1INF50900503
+{
+    internal class Session_05
+    {
+        public static void Main ()
+        {
+
+        }
+        public static void Session05_01()
+        {
+
+        }
+
+
+        
+    }
+}
